dc7d3e96ab575c7ba42013e414eed0200c1a4f64
davidverholen <david@verholen.com>
2014-12-17 21:05:22 +0100

    symfony filesystem->exists does not check for symlinks

--- src/Bragento/Magento/Composer/Installer/Deploy/Strategy/AbstractStrategy.php
+++ src/Bragento/Magento/Composer/Installer/Deploy/Strategy/AbstractStrategy.php
@@ -520,7 +520,7 @@ abstract class AbstractStrategy
                     $this->getDestDir(),
                     $destination
                 );
-                if ($this->getFs()->exists($filePath)) {
+                if ((file_exists($filePath) && is_link($filePath)) || $this->getFs()->exists($filePath)) {
                     $this->removeDelegate($filePath);
                 }
             }
