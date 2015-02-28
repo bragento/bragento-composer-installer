e1932581ff3bdc77fa60793ac2b27fddf87825ed
davidverholen <david@verholen.com>
2015-02-28 11:01:20 +0100

    add composer and iointerface to deployevent

--- src/Bragento/Magento/Composer/Installer/Deploy/Event/DeployEvent.php
+++ src/Bragento/Magento/Composer/Installer/Deploy/Event/DeployEvent.php
@@ -13,8 +13,10 @@
 
 namespace Bragento\Magento\Composer\Installer\Deploy\Event;
 
+use Composer\Composer;
 use Composer\DependencyResolver\Operation\OperationInterface;
 use Composer\EventDispatcher\Event;
+use Composer\IO\IOInterface;
 
 /**
  * Class DeployEvent
@@ -34,16 +36,32 @@ class DeployEvent extends Event
     protected $operation;
 
     /**
+     * @var Composer
+     */
+    protected $composer;
+
+    /**
+     * @var IOInterface
+     */
+    protected $io;
+
+    /**
      * @param string             $name
+     * @param Composer           $composer
+     * @param IOInterface        $io
      * @param OperationInterface $operation
      */
     public function __construct(
         $name,
+        Composer $composer,
+        IOInterface $io,
         OperationInterface $operation
     ) {
         parent::__construct($name);
 
         $this->operation = $operation;
+        $this->composer = $composer;
+        $this->io = $io;
     }
 
     /**
@@ -60,5 +78,37 @@ class DeployEvent extends Event
     public function setOperation($operation)
     {
         $this->operation = $operation;
+    }
+
+    /**
+     * @return Composer
+     */
+    public function getComposer()
+    {
+        return $this->composer;
+    }
+
+    /**
+     * @param Composer $composer
+     */
+    public function setComposer($composer)
+    {
+        $this->composer = $composer;
+    }
+
+    /**
+     * @return IOInterface
+     */
+    public function getIo()
+    {
+        return $this->io;
+    }
+
+    /**
+     * @param IOInterface $io
+     */
+    public function setIo($io)
+    {
+        $this->io = $io;
     }
 }
